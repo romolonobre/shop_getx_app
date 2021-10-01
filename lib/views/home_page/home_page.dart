1438dd106a658bbb70791352eea7b76d64a33256
Romolo Nobre <romoloferreira@icloud.com>
2021-10-01 18:26:15 +0100

    Modifeid $HomePage

--- lib/views/home_page/home_page.dart
+++ lib/views/home_page/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:cart_project/Global_Widgets/custom_buttom.dart';
 import 'package:cart_project/Global_Widgets/custom_buttom_widget.dart';
 import 'package:cart_project/Global_Widgets/get_shop_logo.dart';
+import 'package:cart_project/models/product.dart';
 import 'package:cart_project/views/home_page/components/item_buttons_widget.dart';
 import 'package:cart_project/views/total_page/total_page.dart';
 import 'package:flutter/material.dart';
@@ -15,8 +16,6 @@ import 'components/product_widget.dart';
 /// the UI using $HomePage
 
 class HomePage extends GetView {
-  HomePage({Key? key}) : super(key: key);
-
   // We call CartContoller here to be able to use it
   // on this page. We have to use $Get.put(CartController())
 
@@ -26,110 +25,88 @@ class HomePage extends GetView {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.teal[400],
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            //
-            //GetShop Logo
+      backgroundColor: Colors.teal[400],
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          //
+          //GetShop Logo
 
-            const GetShopLogo(
-              tag: 'tag1',
-            ),
-            const SizedBox(
-              height: 80,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                //
-                //Image product value
+          const GetShopLogo(
+            tag: 'tag1',
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              //
+              // ProductWidget value
 
-                ProductWidget(
-                  imageUrl:
-                      'https://images.unsplash.com/photo-1591195853828-11db59a44f6b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80)',
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
-                //
-                //Image product value
+              ProductWidget(product: controller.shoe),
 
-                ProductWidget(
-                    imageUrl:
-                        'https://images.unsplash.com/photo-1595950653106-6c9ebd614d3a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80')
-              ],
-            ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                // The buttons + and -
-                // we have to call the function we created in the controller
-                // we can do it by calling $controller.increaseShorts().
-                //   -- controller = CartController --
-                ItemButtonsWidget(
-                  onTapIncrease: () =>
-                      controller.increaseShorts(), // Increase Function call
-                  onTapdecrease: () =>
-                      controller.decreaseShorts(), // decrease Function call
-                ),
+              const SizedBox(
+                width: 10,
+              ),
+              //
+              // ProductWidget value
 
-                /// We use $Obx function to acess the
-                /// data from our CartController
+              ProductWidget(product: controller.short),
+            ],
+          ),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              /// We use $Obx function to acess the
+              /// data from our CartController
 
-                Obx(() => Text(
-                      '${controller.shorts.toString()}',
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    )),
-
-                /// We use $Obx function to acess the
-                /// data from our CartController
-
-                Obx(() => Text(
-                      '${controller.shoes.toString()}',
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    )),
-
-                // The buttons + and -
-                // we have to call the function we created in the controller
-                // we can do it by calling $controller.increaseShorts().
-                //   -- controller = CartController --
-
-                ItemButtonsWidget(
-                  onTapIncrease: () =>
-                      controller.increaseShoes(), // Increase Function call
-                  onTapdecrease: () =>
-                      controller.decreaseShoes(), //decrease Function call
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-
-            // We use $Get.to to navigate to the
-            // $TotalPage
-
-            InkWell(
-              onTap: () => Get.to(() => TotalPage()),
-              child: const Hero(
-                tag: 'tag',
-                child: CustomButtomWidget(
-                  text: 'Total',
+              Obx(
+                () => Text(
+                  '${controller.shoes.toString()}',
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
+
+              /// We use $Obx function to acess the
+              /// data from our CartController
+
+              Obx(
+                () => Text(
+                  '${controller.shorts.toString()}',
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+
+          // We use $Get.to to navigate to the
+          // $TotalPage
+
+          InkWell(
+            onTap: () => Get.to(() => TotalPage()),
+            child: const Hero(
+              tag: 'tag',
+              child: CustomButtomWidget(
+                text: 'Total',
+              ),
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
 
     /// THIS IS NOT WORKING
 
