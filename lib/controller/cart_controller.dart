f15f6e8519984886125853ab46f65cc38fe72e00
Romolo Nobre <romoloferreira@icloud.com>
2021-10-01 18:25:11 +0100

    Modified Cartcontroller file

--- lib/controller/cart_controller.dart
+++ lib/controller/cart_controller.dart
@@ -1,3 +1,4 @@
+import 'package:cart_project/models/product.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -8,63 +9,71 @@ class CartController extends GetxController {
   /// you have .obs  near a variable it becames Stateful variable
   /// wich means the variable has some state.
   /// it will redraw the UI immidiately
-  ///
+
   var shorts = 0.obs;
   var shoes = 0.obs;
 
-  /// With $sum variable we cauculate the
-  /// value of the 2 variable we have $shorts + $shoes
-  ///
+  var cart = RxList<Product>();
 
-  int get sum => shorts.value + shoes.value;
+  /// We have created an addToCart function which we
+  /// can use to add a product to the cart
 
-  /// We use this function to Increase the cart
-  /// everytime teh user clic
+  void addToCart(Product product) {
+    cart.add(product);
 
-  void increaseShorts() {
-    shorts++;
+    if (product.name == "Short") {
+      shorts.value++;
+    }
+    if (product.name == "Shoe") {
+      shoes.value++;
+    }
   }
 
-  /// We use this function to decrease the cart
-  /// everytime the user clic.
-  /// We use with the help of $GetX a snackbar to
-  /// show a message to the user if the use try to
-  /// go below 0
-  ///
-  void decreaseShorts() {
-    if (shorts.value <= 0) {
+  /// We have created an addToCart function which we
+  /// can use to remove  a product to the cart
+
+  void removeFromCart(Product product) {
+    if (cart.isEmpty) {
+      showSnackbar();
       return;
     }
-    shorts--;
+    cart.remove(product);
+    if (product.name == "Short") {
+      shorts.value--;
+    }
+    if (product.name == "Shoe") {
+      shoes.value--;
+    }
   }
 
-  /// We use this function to Decrease the cart
-  /// everytime the user clic
+  // List of Products
 
-  void increaseShoes() {
-    shoes++;
-  }
+  final shoe = Product(
+    name: "Shoe",
+    imageUrl:
+        'https://images.unsplash.com/photo-1591195853828-11db59a44f6b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80)',
+  );
+  final short = Product(
+    name: "Short",
+    imageUrl:
+        'https://images.unsplash.com/photo-1595950653106-6c9ebd614d3a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80',
+  );
 
-  /// We use this function to Decrease the cart
-  /// everytime the user clic.
-  /// We use with the help of $GetX a snackbar to
-  /// show a message to the user if the use try to
-  /// go below 0
+  /// We created $showSnackbar fuction to show a snackbar
+  /// every time the use tries to go below 0 item in the cart
   ///
 
-  void decreaseShoes() {
-    if (shoes.value <= 0) {
-      Get.snackbar('GetShop', 'Item can\'t be less than 0',
-          backgroundColor: Colors.blue[200],
-          colorText: Colors.white,
-          duration: const Duration(seconds: 5),
-          icon: const Icon(
-            Icons.notification_important_outlined,
-            color: Colors.white,
-          ));
-
-      return;
-    } //Decrease value
-    shoes--;
+  void showSnackbar() {
+    return Get.snackbar(
+      'GetShop',
+      'Item can\'t be less than 0',
+      backgroundColor: Colors.blue[200],
+      colorText: Colors.white,
+      duration: const Duration(seconds: 5),
+      icon: const Icon(
+        Icons.notification_important_outlined,
+        color: Colors.white,
+      ),
+    );
   }
 }
