a19674bfbe4b94601dd82f8a84fe981a902400ec
Romolo Nobre <romoloferreira@icloud.com>
2021-10-01 18:27:01 +0100

    Modified $ProductWidget

--- lib/views/home_page/components/product_widget.dart
+++ lib/views/home_page/components/product_widget.dart
@@ -1,23 +1,42 @@
+import 'package:cart_project/controller/cart_controller.dart';
+import 'package:cart_project/models/product.dart';
+import 'package:cart_project/views/home_page/components/item_buttons_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 /// This widget draws the iten as a image.
 /// a imageUrl is required
 
 class ProductWidget extends StatelessWidget {
-  ProductWidget({required this.imageUrl});
+  ProductWidget({required this.product});
 
-  final String imageUrl;
-
+  final Product product;
+  CartController controller = Get.find();
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(20),
-      child: Image.network(
-        imageUrl,
-        fit: BoxFit.cover,
-        height: 120,
-        width: 165,
-      ),
+    return Column(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Image.network(
+            product.imageUrl,
+            fit: BoxFit.cover,
+            height: 120,
+            width: 165,
+          ),
+        ),
+
+        /// This Widget draws our buttons : + and -
+        /// the buttons has to paramentes
+        /// onTapIncrease (increase item cart)
+        /// and onTapdecrease(decrease item cart)
+
+        ItemButtonsWidget(
+          onTapIncrease: () =>
+              controller.addToCart(product), // Increase Function call
+          onTapdecrease: () => controller.removeFromCart(product),
+        ),
+      ],
     );
   }
 }
